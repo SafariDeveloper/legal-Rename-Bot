49d0144333d238f4c8aee0b96dd9bad31d16272f
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-08-05 14:00:48 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from pytz import timezone
 from config import Config, Txt 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import asyncio
+
 
 
 async def progress_for_pyrogram(current, total, ud_type, message, start):
@@ -80,5 +82,24 @@ async def send_log(b, u):
         )
         
 
+async def get_verify_status(user_id):
+    verify = await db.get_verify_status(user_id)
+    return verify
 
+async def update_verify_status(user_id, verify_token="", is_verified=False, verified_time=0, link=""):
+    current = await get_verify_status(user_id)
+    current['verify_token'] = verify_token
+    current['is_verified'] = is_verified
+    current['verified_time'] = verified_time
+    current['link'] = link
+    await db.update_verify_status(user_id, current)
+    
+def get_readable_time(seconds):
+    periods = [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]
+    result = ''
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            result += f'{int(period_value)}{period_name}'
+    return result
 
